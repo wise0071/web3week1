5930a1e7e204e88f95f10ec99bcb33c525152f01
qwiz <wise0071@algonquinlive.com>
2025-05-08 13:41:49 -0400

    end of class

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,10 @@
 //alert("whats up");
 //alert for pop up
 console.log("hello");
+
+//tell the first paragraph p1 to listen for a click
+
+document.getElementById("p1").addEventListener("click", function(){
+    //change text of p2 once clicked
+    document.getElementById("p2").textContent = "you clicked on the first paragraph";
+});
